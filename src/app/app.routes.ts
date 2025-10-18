7389aa0270456c7a291a4a3b5cb3e1190bdd8e29
Marc DIBEH <dibemark.job@gmail.com>
2025-10-18 14:41:58 +0200

    add route for HubSpot callback component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
+import { HubspotCallbackComponent } from './components/hubspot-callback/hubspot-callback.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'auth/callback', component: AuthCallbackComponent },
+  { path: 'auth/hubspot/callback', component: HubspotCallbackComponent },
   { path: 'chat', component: ChatComponent },
   { path: '**', redirectTo: '/login' },
 ];
